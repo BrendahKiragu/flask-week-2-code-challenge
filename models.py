6c55cee207616d4e14e2af39df61c86bc423f04f
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-10-12 15:41:31 +0300

    adds table names

--- models.py
+++ models.py
@@ -5,19 +5,27 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Episode(db.Model, SerializerMixin):
+    __tablename__ = "episodes"
+
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.String)
     number = db.Column(db.Integer)
+
+    serialize_rules = ("-appearances.episode")
 
     #relationships
     appearances = db.relationship('Appearance', back_populates='episode')
     guests = AssociationProxy('appearances', 'guest', creator=lambda guestObj: Appearance(guest=guestObj))
 
 class Appearance(db.Model, SerializerMixin):
+    __tablename__ = "appearances"
+
     id = db.Column(db.Integer, primary_key=True) 
     rating = db.Column(db.Integer) 
-    episode_id = db.Column(db.Integer, db.ForeignKey('episode.id'))  # Foreign key reference to Episode
-    guest_id = db.Column(db.Integer, db.ForeignKey('guest.id'))  # Foreign key reference to Guest
+    episode_id = db.Column(db.Integer, db.ForeignKey('episodes.id'))  # Foreign key reference to Episode
+    guest_id = db.Column(db.Integer, db.ForeignKey('guests.id'))  # Foreign key reference to Guest
+
+    serialize_rules = ("-episode.appearances", "-guest.apearances")
 
     # relationships
     episode = db.relationship('Episode', back_populates='appearances')
@@ -25,9 +33,13 @@ class Appearance(db.Model, SerializerMixin):
 
 
 class Guest(db.Model, SerializerMixin):
+    __tablename__ = "guests"
+
     id = db.Column(db.Integer, primary_key=True)  
     name = db.Column(db.String) 
     occupation = db.Column(db.String) 
+
+    serialize_rules = ("-appearances.guest")
 
     #relationships
     appearances = db.relationship('Appearance', back_populates='guest')
