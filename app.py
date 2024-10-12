e8197434793547a58dbab40df110ece42bbd9ad1
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-10-12 14:58:41 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
+
+from models import db
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
+@app.route('/')
+def index():
+    return "Hello"
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=5555)
