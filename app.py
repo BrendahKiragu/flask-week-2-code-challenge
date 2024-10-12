995c2418b3e00ec1ae81ede40e8dd164e6fc5b1f
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-10-12 20:05:07 +0300

    creates a new appearance

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, request
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
@@ -31,31 +31,53 @@ class Episodes(Resource):
             }
             episodes_list.append(episode_dict)
 
-        return make_response(episodes_list, 200)  
+        return make_response(jsonify(episodes_list), 200)  
 
 class EpisodeID(Resource):
     def get(self, id):
         episode = Episode.query.filter(Episode.id == id) .first()  
 
         if episode:
-            return make_response(episode.to_dict(), 200) 
+            return make_response(jsonify(episode.to_dict()), 200) 
     
-        return {'error': "Episode not found"}, 404
+        return make_response(jsonify({'error': "Episode not found"}), 404)
     
 class Guests(Resource):
     def get(self):
         guests = [guest.to_dict() for guest in Guest.query.all()]
 
-        return make_response(guests, 200)
+        return make_response(jsonify(guests), 200)
 
-class Appearance(Resource):
-    def get(self):
-        pass
+class Appearances(Resource):
+    def post(self):
+        data = request.json
+        rating = data.get('rating')
+        episode_id = data.get('episode_id')
+        guest_id = data.get('guest_id')
+
+        if rating is None or not (1 <= rating <=5) : #validates rating
+            return make_response(jsonify({'error': "Rating must be between 1 and 5!"}), 400) 
+        
+        if episode_id is None or guest_id is None:
+            return make_response(jsonify({'error': "Missing required fields!!!"}), 400)
+                           
+         #create a new appearance if all checks pass
+        if Episode.query.filter_by(id = episode_id).first() and Guest.query.filter_by(id = guest_id).first():
+            new_appearance = Appearance(
+                rating = rating, 
+                episode_id = episode_id,
+                guest_id= guest_id)
+            
+            db.session.add(new_appearance)
+            db.session.commit()
+            return make_response(jsonify(new_appearance.to_dict()), 201)
+        else:
+            return make_response(jsonify({'error': "Invalid episode_id or guest_id"}), 404)
 
 api.add_resource(Episodes, '/episodes')
 api.add_resource(EpisodeID, '/episodes/<int:id>')
 api.add_resource(Guests, '/guests')
-# api.add_resource(Appearance, '/appearances')
+api.add_resource(Appearances, '/appearances')
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=5555)
+    app.run(port=5555, debug=True)
